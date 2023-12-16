41ef76cbdfb44f0b62449baa07ecbf69f8dfdfde
Borislav Blazhev <b.d.blazhev@gmail.com>
2023-12-16 16:13:54 +0200

    Added FileProcessController

---
+++ FileUploadApp.API/Controllers/FileProcessController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FileUploadApp.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FileProcessController : ControllerBase
+    {
+        private readonly ILogger<FileProcessController> _logger;
+
+        public FileProcessController(ILogger<FileProcessController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile file, string filename)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
