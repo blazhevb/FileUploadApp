5422cad0af2708be5cca985fc8de1b4b7445bc36
Borislav Blazhev <b.d.blazhev@gmail.com>
2023-12-17 00:00:29 +0200

    Add filesystem manager

---
+++ FileUploadApp.Contracts/FileSystem/IFileSystemManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileUploadApp.Contracts.FileSystem
+{
+    public interface IFileSystemManager
+    {
+        Task SaveFileToDiskAsync(string content);
+    }
+}
---
+++ FileUploadApp.Implementation/FileSystem/FileSystemManager.cs
@@ -0,0 +1,18 @@
+﻿using FileUploadApp.Contracts.FileSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileUploadApp.Implementation.FileSystem
+{
+    public class FileSystemManager : IFileSystemManager
+    {
+        public async Task SaveFileToDiskAsync(string content)
+        {
+
+            await File.WriteAllTextAsync("", content, Encoding.UTF8);
+        }
+    }
+}
